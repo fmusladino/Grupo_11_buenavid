fe0217c65af99cd469a4fde0f41fd5a31d265fd4
BautiLentz <bautistalentz@gmail.com>
2022-11-04 14:44:32 -0300

    agregando en rutas el metodo POST

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -7,12 +7,22 @@ const mainController = require('../controller/mainController');
 
 router.get('/', mainController.controller.index);
 
+router.post('/', mainController.controller.index);
+
 router.get('/login', mainController.controller.login);
+
+router.post('/login', mainController.controller.login);
 
 router.get('/carrito', mainController.controller.productCar);
 
+router.post('/carrito', mainController.controller.productCar);
+
 router.get('/producto', mainController.controller.productDetail);
 
+router.post('/producto', mainController.controller.productDetail);
+
 router.get('/registro', mainController.controller.register);
+
+router.post('/registro', mainController.controller.register);
 
 module.exports = router;
