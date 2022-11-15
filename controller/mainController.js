0da6b5f6066e6756842501ac423f315f159f9f28
fmusladino@hotmail.com <fmusladino@hotmail.com>
2022-11-15 13:20:34 -0300

    arregle iconos header

--- controller/mainController.js
+++ controller/mainController.js
@@ -5,28 +5,28 @@ const products = JSON.parse (fs.readFileSync("./data/productos.json"));
 
 const controller = {
     index: (req,res) => {
-        return res.render (path.resolve('./views/index.ejs'))
+        return res.render ('index')
     },
     login: (req,res) => {
-        return res.render (path.resolve('./views/login.ejs'))
+        return res.render ('login')
     },
     productCar: (req,res) => {
-        return res.render (path.resolve('./views/productCar.ejs'))
+        return res.render ('productCar')
     },
     productDetail: (req,res) => {
         const productToEdit = products[0]
         return res.render ('productDetail', {product:productToEdit})
     },
     register: (req,res) => {
-        return res.render (path.resolve('./views/register.ejs'))
+        return res.render ('register')
     },
 
     carga: (req,res) => {
-        return res.render (path.resolve('./views/formCarga.ejs'))
+        return res.render ('formCarga')
     },
 
     edicion: (req,res) => {
-        return res.render (path.resolve('./views/formEdicion.ejs'))
+        return res.render ('formEdicion')
     },
   
 
