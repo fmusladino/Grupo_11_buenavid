47b67323c7695f049629ca262d0b4d4df7ea1d2a
Aleto67 <aleto67@gmail.com>
2022-11-21 15:56:28 -0300

    Instalación Express-Valitador y armado de array de validadcion en ruta

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -2,6 +2,29 @@ const express = require ('express');
 const { controller }  = require('../controller/mainController');
 const router = express.Router();
 const multer = require('multer');
+const {check} = require('express-validator');
+
+const validateRegister = [
+
+    check('description')
+        .notEmpty().withMessage('Debes completar con la descripción').bail
+        .isLength({min: 10}).withMessage('la descripción debe tener como mínimo 10 caractéres'),
+    check('winery')
+        .notEmpty().withMessage('Debes completar la bodega').bail
+        .isLength({min: 3}).withMessage('la bodega debe tener como mínimo 3 caractéres'),
+    check('origin')
+        .notEmpty().withMessage('Debes completar el origen regional').bail
+        .isLength({min: 5}).withMessage('El origen regional debe tener como mínimo 5 caractéres'),
+    check('year')
+        .notEmpty().withMessage('Debes completar el año').bail
+        .isLength({min: 4}).withMessage('El año debe tener como mínim 4 caractéres'),
+    check('price')
+        .notEmpty().withMessage('Debes completar el precio').bail
+        .isLength({min: 3}).withMessage('El precio debe tener como mínimo 3 caractéres (numércios, además del "."'),
+    check('discount')
+        .notEmpty().withMessage('Debes completar el descuento - con 0 si no hubiera descuento').bail
+        
+];
 
 const storage = multer.diskStorage({
         destination:    function (req, file, cb) {
